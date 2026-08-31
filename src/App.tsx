a6c6fc329547ffbd7c1e3ee6fb68c19a6dc7be3f
ManuelCuellar <cuellar@udel.edum>
2026-08-30 21:53:51 -0400

    Complete HTML and CSS task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>UD CISC275 with React Hooks and TypeScript</h1>
                 <p>Manuel Cuellar Montenegro</p>
             </header>
+
             <p>Hello World</p>
+
+            <img
+                src={`${process.env.PUBLIC_URL}/logo192.png`}
+                alt="React logo"
+            />
+
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>TypeScript</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
