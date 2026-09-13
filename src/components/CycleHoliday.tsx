fabea5e4417c0b78d987b36d72a2550085ec0577
Manuel Cuellar <cuellar@udel.edu>
2026-09-12 22:00:45 -0400

    Completed state task

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,56 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "Christmas"
+    | "Halloween"
+    | "New Year"
+    | "Thanksgiving"
+    | "Valentines";
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("Christmas");
+    const holidayEmoji: Record<Holiday, string> = {
+        Christmas: "🎄",
+        Halloween: "🎃",
+        "New Year": "🎉",
+        Thanksgiving: "🦃",
+        Valentines: "❤️",
+    };
+    const byYear: Record<Holiday, Holiday> = {
+        "New Year": "Valentines",
+        Valentines: "Halloween",
+        Halloween: "Thanksgiving",
+        Thanksgiving: "Christmas",
+        Christmas: "New Year",
+    };
+    const byAlphabet: Record<Holiday, Holiday> = {
+        Christmas: "Halloween",
+        Halloween: "New Year",
+        "New Year": "Thanksgiving",
+        Thanksgiving: "Valentines",
+        Valentines: "Christmas",
+    };
+
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <Button
+                onClick={() => {
+                    setHoliday(byAlphabet[holiday]);
+                }}
+            >
+                Advance by Alphabet
+            </Button>
+            ;
+            <Button
+                onClick={() => {
+                    setHoliday(byYear[holiday]);
+                }}
+            >
+                Advance by Year
+            </Button>
+            ;
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [quiz, setQuiz] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button
+                disabled={quiz || attempts === 0}
+                onClick={() => {
+                    setQuiz(true);
+                    setAttempts(attempts - 1);
+                }}
+            >
+                Start Quiz
+            </Button>
+
+            <Button
+                disabled={quiz}
+                onClick={() => {
+                    setAttempts(attempts + 1);
+                }}
+            >
+                Mulligan
+            </Button>
+
+            <Button
+                disabled={!quiz}
+                onClick={() => {
+                    setQuiz(false);
+                }}
+            >
+                Stop Quiz
+            </Button>
+
+            <span>Attempts: {attempts}</span>
+        </div>
+    );
 }
