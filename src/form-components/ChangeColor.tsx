e2bf8050817ce3efa8d0bd75e319b7390596c4af
Manuel Cuellar <cuellar@udel.edu>
2026-09-20 19:06:09 -0400

    Complete Forms

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
 
+const COLORS = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "orange",
+    "purple",
+    "pink",
+    "gray",
+];
+
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>(COLORS[0]);
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
+
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((currentColor) => (
+                <label key={currentColor}>
+                    <input
+                        type="radio"
+                        name="colors"
+                        value={currentColor}
+                        onChange={updateColor}
+                        checked={color === currentColor}
+                    />
+                    {currentColor}
+                </label>
+            ))}
+            <div
+                data-testid="colored-box"
+                style={{
+                    backgroundColor: color,
+                    width: "100px",
+                    height: "100px",
+                }}
+            >
+                {color}
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <select value={answer} onChange={updateAnswer}>
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+
+            <p>{answer === expectedAnswer ? "✔️" : "❌"}</p>
         </div>
     );
 }
